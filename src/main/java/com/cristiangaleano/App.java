874ae769d664c627cd1ab7b3820546795c4b75bc
CristianSGaleanoB <stivenbarbosa62@gmail.com>
2025-09-23 23:18:09 -0500

    First commit

---
+++ src/main/java/com/cristiangaleano/App.java
@@ -0,0 +1,13 @@
+package com.cristiangaleano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
